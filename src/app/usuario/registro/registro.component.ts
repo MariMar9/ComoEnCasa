96183b302dab41f79ad6b66f571dbff17d04f36a
karilain02 <sara.karilain@gmail.com>
2021-04-28 21:15:26 +0200

    push

--- src/app/usuario/registro/registro.component.ts
+++ src/app/usuario/registro/registro.component.ts
@@ -22,7 +22,7 @@ export class RegistroComponent implements OnInit {
   constructor(public auth: AngularFireAuth, firestore: AngularFirestore, private _GuardarUsuarioService: GuardarUsuarioService) {
     /*Rellena la variable usuarios con una colección de tipo usuarios*/
     this.usuarios = firestore.collection('usuarios').valueChanges();
-
+    console.log( this.usuarios+"hola")
   }
 
   ngOnInit(): void {
@@ -105,9 +105,9 @@ export class RegistroComponent implements OnInit {
           this._GuardarUsuarioService.agregarUsuario(usuario)
             .then(() => console.log("Usuario registrado con éxito."))
             .catch(error => {console.log(error)});
-        })
+        })/*
         .then(() => (
-          window.location.href = '/inicio'))
+          window.location.href = '/inicio'))*/
         .catch((error) => {
           if (error.message == 'The email address is badly formatted.') {
             (<HTMLInputElement>(
