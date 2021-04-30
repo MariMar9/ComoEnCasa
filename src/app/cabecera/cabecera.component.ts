fb8a139bd5ea1e25d9b01e8d48fd60f2cbc91d02
karilain02 <sara.karilain@gmail.com>
2021-04-30 21:34:02 +0200

    sesiones ya casi

--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CargarScriptsService } from '../services/cargar-scripts.service';
 
 @Component({
   selector: 'app-cabecera',
   templateUrl: './cabecera.component.html',
-  styleUrls: ['./cabecera.component.css']
+  styleUrls: ['./cabecera.component.css'],
 })
 export class CabeceraComponent implements OnInit {
+  usuarioConectado = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _CargaScripts: CargarScriptsService) {
+    this.usuarioConectado = _CargaScripts.conectado();
   }
 
+  ngOnInit(): void {}
 }
