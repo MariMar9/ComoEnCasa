0f57e4bcae0d4b77f60fd6515bc9198289923737
MariMar9 <garciacarrionmar@gmail.com>
2021-05-05 22:40:14 +0200

    mostrar receta completo

--- src/app/recetasVista/mostrar-receta/mostrar-receta.component.ts
+++ src/app/recetasVista/mostrar-receta/mostrar-receta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { RecetasService } from '../../services/recetas.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-mostrar-receta',
@@ -13,22 +14,20 @@ export class MostrarRecetaComponent implements OnInit {
   recetas: Observable<any[]>;
   idReceta: number=0;
 
-  constructor(public firestore: AngularFirestore, private _pasarReceta: RecetasService) {
+  constructor(public firestore: AngularFirestore, private _pasarReceta: RecetasService, private router:Router) {
     this.recetas = firestore.collection('recetas').valueChanges();
      /*Con el método "subscribe" recibe el dato que manda la función "mandarCategoria".*/
     this._pasarReceta.mandarReceta.subscribe(idReceta=>{
     this.idReceta=idReceta;
-      console.log("Id: "+idReceta)
-      console.log("Id: "+this.idReceta)
-    });
+    })
+    setTimeout(() => {
+      if (this.idReceta==0) {
+        this.router.navigate(['/recetas']);
+      }
+    }, 300);
   }
 
   ngOnInit(): void {
-  }
-
-  redirigir(){
-    location.href="/recetas";
-    return null;
   }
 
 }
