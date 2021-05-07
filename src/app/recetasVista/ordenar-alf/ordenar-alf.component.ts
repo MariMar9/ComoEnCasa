5b381cde4fb6a3d2d93669708d8a537fb7ed2411
karilain02 <sara.karilain@gmail.com>
2021-05-07 17:54:28 +0200

    oredenar recetas mejora

--- src/app/recetasVista/ordenar-alf/ordenar-alf.component.ts
+++ src/app/recetasVista/ordenar-alf/ordenar-alf.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { RecetasService } from 'src/app/services/recetas.service';
 
 @Component({
   selector: 'app-ordenar-alf',
@@ -12,39 +13,25 @@ export class OrdenarAlfComponent implements OnInit {
   recetasOrdenadas = [''];
   recetasFirebase: Observable<any[]>;
 
-  constructor(public firestore: AngularFirestore) {
+  constructor(public firestore: AngularFirestore,private _pasarReceta: RecetasService) {
     this.recetasFirebase = firestore.collection('recetas').valueChanges();
+    //firestore.firestore.collection('recetas').orderBy('nombre')
     this.recetasFirebase.forEach((receta) => {
       for (let i = 0; i < receta.length; i++) {
         for (let j = receta.length - 1; j > i; j--) {
-          this.recetasOrdenadas[i] = receta[j].nombre;
+          this.recetasOrdenadas[i] = receta[j].nombre;;
         }
       }
-
       /**ordenamos las recetas con primero las ordemamos con .sort() y con .map() obtenemos ek nuevo array */
-      this.recetasOrdenadas.sort().map(function (recetas) {
-        (<HTMLInputElement>document.getElementById('recetas')).innerHTML += `
-        <div class="my-3 p-3 border border-gray" style="background: #f4f9ff;border-radius: 7px;">
-            <div class="row receta-contenedor d-flex justify-content-between align-items-center">
-                <div class="receta-nombre-icono d-flex align-items-center col-12 col-md-10 col-lg-10">
-                    <div class="icon-fab"><i class="fas fa-book-open" style="color: #fff;background: #757575;padding: 8px;border-radius: 5px;"></i></div>
-                    <div class="nombre">
-                        <p class="text-gray-dark m-0 ml-5">${recetas}</p>
-                    </div>
-                </div>
-  
-                <div class="ver-receta col-12 col-md-2 col-lg-2" style="text-align: center">
-                    <a href="#">Ver receta</a>
-                </div>
-            </div>
-        </div>
-      `;
-
-        /* (<HTMLInputElement>(
-            document.getElementById('recetas')
-          )).innerHTML += `<p class="m-0 pt-4 pb-4 border-top border-bottom"><a href='#' class="">${recetas}</a></p>`;*/
-      });
+      this.recetasOrdenadas.sort()
     });
+  }
+
+  mandarRecetaNombre(idReceta: string) {
+    console.log("manda"+" "+idReceta)
+    setTimeout(() => {
+      this._pasarReceta.mandarReceta.emit(idReceta);
+    }, 200);
   }
 
   ngOnInit(): void {}
--- src/app/services/recetas.service.ts
+++ src/app/services/recetas.service.ts
@@ -7,6 +7,7 @@ export class RecetasService {
   /*Pasa datos del component Cabecera al component Recetas.*/
   @Output() mandarCategoria: EventEmitter<any> = new EventEmitter();
   @Output() mandarReceta: EventEmitter<any> = new EventEmitter();
+  @Output() mandarRecetaNombre: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
