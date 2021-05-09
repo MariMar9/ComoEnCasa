3c7a792443be30fa1d46d26cb5550a413a72867b
MariMar9 <garciacarrionmar@gmail.com>
2021-05-09 16:42:00 +0200

    crear receta incompleto 2

--- src/app/recetasVista/crear-receta/crear-receta.component.ts
+++ src/app/recetasVista/crear-receta/crear-receta.component.ts
@@ -17,14 +17,28 @@ export class CrearRecetaComponent implements OnInit {
   numIngrediente: number=2;
   numPaso: number=2;
 
-  /*imagen: File=new File(["foo"], "foo.txt", {
+  /*Variables de las recetas*/
+  /*imagenReceta: File=new File(["foo"], "foo.txt", {
     type: "text/plain",
   });*/
-  nombre: string="";
+  idReceta: number=0;
+  nombreReceta: string="";
   categoria: string="Seleccione una categoría";
   comensales: number=0;
   dificultad: string="Dificultad";
   duracion: string="";
+
+  /*Variables de los ingredientes*/
+  idIngrediente: number=0;
+  nombreIngrediente: string="";
+  cantidad: string="";
+
+  /*Variables de los pasos*/  
+  idPaso: number=0;
+  descripcion: string="";
+  /*imagenPaso: File=new File(["foo"], "foo.txt", {
+    type: "text/plain",
+  });*/
 
   constructor(public firebaseAuth: AngularFireAuth, public firestore: AngularFirestore, private _CargaScripts: CargarScriptsService, private _recibirCorreoUsuario: GuardarUsuarioService) {
     this.quitar = _CargaScripts.quitarCabFoot();
@@ -51,7 +65,6 @@ export class CrearRecetaComponent implements OnInit {
     nuevoIngrediente.setAttribute("style", "display:flex;justify-content: space-between;");
     nuevoIngrediente.setAttribute("class", "ingrediente form-group");
     
-
     /**crea los input de los ingredientes*/
     let inputNuevoIngrediente=document.createElement("input");
     inputNuevoIngrediente.setAttribute("placeholder","ingrediente "+this.numIngrediente)
@@ -66,9 +79,6 @@ export class CrearRecetaComponent implements OnInit {
     inputNuevaCantidad.setAttribute("class","form-control ingrediente-input")
 
     nuevoIngrediente.appendChild(inputNuevoIngrediente);
-    /*Mirar cómo añadir texto2 aquí.
-    let texto2=document.createTextNode("Cantidad: ");
-    inputNuevoIngrediente.appendChild(texto2);*/
     nuevoIngrediente.appendChild(inputNuevaCantidad);
 
     let ingredientes=document.getElementById("ingredientes");
@@ -105,7 +115,7 @@ export class CrearRecetaComponent implements OnInit {
 
   validarDatos(){
     let correcto=true;
-    if (this.nombre=="") {
+    if (this.nombreReceta=="") {
       console.log("Falta nombre");
       correcto=false;
     }
@@ -113,10 +123,11 @@ export class CrearRecetaComponent implements OnInit {
       console.log("Falta categoría");
       correcto=false;
     }
-    if (this.comensales==0 || this.comensales==null) {
+    if (this.comensales<=0 || this.comensales==null) {
       console.log("Falta comensales");
       correcto=false;
     }
+    console.log(this.comensales);
     if (this.dificultad=="Dificultad") {
       console.log("Falta dificultad");
       correcto=false;
@@ -126,25 +137,96 @@ export class CrearRecetaComponent implements OnInit {
       correcto=false;
     }
 
+    /*Ingredientes vacíos*/
+    var ingredientes=document.getElementsByClassName("ingrediente");
+    for (let i = 0; i < ingredientes.length; i++) {
+      if ((<HTMLInputElement>ingredientes[i].children[0]).value=="") {
+        console.log("vacío ingrediente "+i);
+      }
+    }
+
+    /*Cantidades vacías*/
+    for (let i = 0; i < ingredientes.length; i++) {
+      if ((<HTMLInputElement>ingredientes[i].children[1]).value=="") {
+        console.log("vacía cantidad "+i);
+      }
+    }
+
+    /*Pasos vacíos*/
+    var pasos=document.getElementsByClassName("paso");
+    for (let i = 0; i < pasos.length; i++) {
+      if ((<HTMLInputElement>pasos[i].children[0]).value=="") {
+        console.log("vacío paso "+i);
+      }
+    }
+
     if (correcto) {
       this.aniadirReceta();
     }
   }
 
   aniadirReceta(){
+    /*Contar las recetas que hay para asignar un id a la nueva receta. Y lo mismo con ingredientes y pasos.
+    var recetas = this.firestore.collection('recetas').valueChanges();
+    var idReceta=0;
+    recetas.forEach(receta => {
+      idReceta++;
+    });
+    console.log(idReceta);*/
+    /*Añadir la nueva receta a firebase*/
     this.firestore.collection("recetas").add({
-      nombre: this.nombre,
+      id: this.idReceta,
+      correo_usuario: this.correo,
+      nombre: this.nombreReceta,
       categoria: this.categoria,
       comensales: this.comensales,
       dificultad: this.dificultad,
-      duracion: this.duracion
+      duracion: this.duracion,
+      fecha: new Date()
     })
     .then((recetaCreada) => {
       console.log("Se ha creado la receta.");
     })
     .catch((error) => {
-        console.error("Se ha producido un error.");
+        console.error("Se ha producido un error al crear la receta.");
     });
+    
+    /*Añadir los nuevos ingredientes a firebase*/
+    var ingredientes=document.getElementsByClassName("ingrediente");
+    for (let i = 0; i < this.numIngrediente-1; i++) {
+      this.nombreIngrediente=(<HTMLInputElement>ingredientes[i].children[0]).value;
+      this.cantidad=(<HTMLInputElement>ingredientes[i].children[1]).value;
+      this.firestore.collection("ingredientes").add({
+        id: this.idIngrediente,
+        nombre: this.nombreIngrediente,
+        cantidad: this.cantidad,
+        idReceta: this.idReceta
+      })
+      .then((ingredienteCreado) => {
+        console.log("Se ha creado el ingrediente.");
+      })
+      .catch((error) => {
+          console.error("Se ha producido un error al crear el ingrediente.");
+      });
+    }
+    
+    
+    /*Añadir los nuevos pasos a firebase*/
+    var pasos=document.getElementsByClassName("paso");
+    for (let i = 0; i < this.numPaso-1; i++) {
+      this.descripcion=(<HTMLInputElement>pasos[i].children[0]).value;
+      this.firestore.collection("pasos").add({
+        id: this.idPaso,
+        descripcion: this.descripcion,
+        idReceta: this.idReceta
+      })
+      .then((pasoCreado) => {
+        console.log("Se ha creado el paso.");
+      })
+      .catch((error) => {
+          console.error("Se ha producido un error al crear el paso.");
+      });
+    }
   }
 
 }
