d8eb03bd2ba83f5881b933ea5fa19f466371f2a6
karilain02 <sara.karilain@gmail.com>
2021-05-17 17:14:08 +0200

    buscador completado

--- src/app/core/cabecera/cabecera.component.ts
+++ src/app/core/cabecera/cabecera.component.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { CargarScriptsService } from '../services/cargar-scripts.service';
 import { RecetasService } from '../services/recetas.service';
+import {Router} from '@angular/router'; 
 
 @Component({
   selector: 'app-cabecera',
@@ -14,14 +15,18 @@ export class CabeceraComponent implements OnInit {
   quitar = false;
   pasa = true;
   categoria: string = '';
+  recetas: Observable<any[]>;
   constructor(
     public firestore: AngularFirestore,
     private _CargaScripts: CargarScriptsService,
     private _pasarCategoria: RecetasService,
-    private _pasarReceta: RecetasService
+    private _pasarReceta: RecetasService,
+    private route:Router
   ) {
     this.quitar = _CargaScripts.quitarCabFoot();
     this.usuarioConectado = _CargaScripts.conectado();
+    this.recetas = this._pasarReceta.getCollectionRecetas<any>('/recetas','id')
+
   }
 
   ngOnInit(): void {}
@@ -34,25 +39,52 @@ export class CabeceraComponent implements OnInit {
   }
 
   pasarReceta() {
-    var nombre = (<HTMLInputElement>document.getElementById('buscar-receta'))!
-      .value;
+    var nombre = '';
+    var cuerpo = '';
+    var primeraLetra = '';
+    var array = ['']
+    primeraLetra =(<HTMLInputElement>document.getElementById('buscar-receta'))!.value.charAt(0).toUpperCase()
+    cuerpo = (<HTMLInputElement>document.getElementById('buscar-receta'))!.value.substring(1).toLocaleLowerCase();
+    this.recetas.subscribe((receta)=>{
+      for (let i = 0; i < receta.length; i++) {
+       array [i]= receta[i].nombre
+       if(array[i].includes(primeraLetra.toLowerCase()+cuerpo)){
+          primeraLetra =(<HTMLInputElement>document.getElementById('buscar-receta'))!.value.charAt(0).toLowerCase()
+          nombre =primeraLetra+cuerpo
+          nombre.toLowerCase() 
+       }
+      }  
+    })
+    nombre=primeraLetra+cuerpo
+
     setTimeout(() => {
       this._pasarReceta.mandarReceta.emit(nombre);
     }, 200);
   }
 
-  buscar(e: Event) {
+  buscar(e: KeyboardEventInit) {
     var nombre = '';
+    var cuerpo = '';
     var primeraLetra = '';
-    if (nombre != '' || nombre != null) {
-      primeraLetra =(<HTMLInputElement>document.getElementById('buscar-receta')).value.charAt(0).toUpperCase()
-      nombre = primeraLetra +(<HTMLInputElement>document.getElementById('buscar-receta')).value.substring(1).toLocaleLowerCase();
+    var array = ['']
+    if(e.key=="Enter"){
+
+      primeraLetra =(<HTMLInputElement>document.getElementById('buscar-receta'))!.value.charAt(0).toUpperCase()
+      cuerpo = (<HTMLInputElement>document.getElementById('buscar-receta'))!.value.substring(1).toLocaleLowerCase();
+      this.route.navigate(['/buscarReceta'])
+      this.recetas.subscribe((receta)=>{
+        for (let i = 0; i < receta.length; i++) {
+         array [i]= receta[i].nombre
+         if(array[i].includes(primeraLetra.toLowerCase()+cuerpo)){
+            primeraLetra =(<HTMLInputElement>document.getElementById('buscar-receta'))!.value.charAt(0).toLowerCase()
+            nombre =primeraLetra+cuerpo
+            nombre.toLowerCase() 
+         }
+        }  
+      })
+      nombre=primeraLetra+cuerpo
     }
-
    
-
-    
-
     setTimeout(() => {
       this._pasarReceta.mandarReceta.emit(nombre);
     }, 200);
