e09b437bff1fd92d2d7741f7389bfea3675a7772
karilain02 <sara.karilain@gmail.com>
2021-05-24 01:31:52 +0200

    colores

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -6,23 +6,36 @@ import { CargarScriptsService } from '../../core/services/cargar-scripts.service
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
-  nombreReceta: string="";
+  nombreReceta: string = '';
   recetas: Observable<any[]>;
-  usuarioConectado=false;
-  
-  constructor(private _CargaScripts: CargarScriptsService,private _pasarReceta: RecetasService) {
+  usuarioConectado = false;
+  color: string = '';
+  constructor(
+    private _CargaScripts: CargarScriptsService,
+    private _pasarReceta: RecetasService
+  ) {
     this.usuarioConectado = _CargaScripts.conectado();
-    this.recetas = this._pasarReceta.getCollectionRecientes<any>('recetas/','fecha');
-    this._pasarReceta.mandarReceta.subscribe(nombre=>{
-      this.nombreReceta=nombre;
+    this.recetas = this._pasarReceta.getCollectionRecientes<any>(
+      'recetas/',
+      'fecha'
+    );
+    this._pasarReceta.mandarReceta.subscribe((nombre) => {
+      this.nombreReceta = nombre;
     });
   }
 
   ngOnInit(): void {
-
+    setTimeout(() => {
+      var num = 1;
+      let boton = document.querySelectorAll<HTMLElement>('.btn-ver');
+      boton.forEach((bot) => {
+        bot.setAttribute('class', 'btn btn-ver' +num);
+        num++;
+      });
+    }, 500);
   }
   pasarReceta(idReceta: number) {
     setTimeout(() => {
